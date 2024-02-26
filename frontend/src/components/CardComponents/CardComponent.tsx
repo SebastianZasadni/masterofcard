c26686476fbe6f2e718e1c5b7ae5350c6ba24320
SebastianZasadni <emblelele@gmail.com>
2024-02-26 09:13:09 +0100

    configure project; feature card-component

---
+++ frontend/src/components/CardComponents/CardComponent.tsx
@@ -0,0 +1,25 @@
+import css from "./CardComponents.module.css";
+
+const CardComponent = ({
+  letter = "9",
+  symbol = "trefl",
+}: {
+  letter: string;
+  symbol: string;
+}) => {
+  return (
+    <div className={css.cardComponent__container}>
+      <div className={css.cardLetterAndSymbol__box}>
+        <p className={`${css.cardLetter} ${css[symbol]}`}>{letter}</p>
+        <svg className={`${css.cardIcon}`}>
+          <use href={`/assets/icons.svg#${symbol}`} />
+        </svg>
+      </div>
+      <svg className={`${css.cardIconMiddle}`}>
+        <use href={`/assets/icons.svg#${symbol}`} />
+      </svg>
+    </div>
+  );
+};
+
+export default CardComponent;
